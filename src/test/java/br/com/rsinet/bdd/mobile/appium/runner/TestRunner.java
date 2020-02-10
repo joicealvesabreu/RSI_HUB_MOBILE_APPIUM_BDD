cc02a0445ba56f54849e51037ae1391276e2c31b
Joice Natalice Alves De Abreu <joice.abreu@rsinet.com.br>
2020-02-10 13:25:11 -0300

    pom.xml

--- src/test/java/br/com/rsinet/bdd/mobile/appium/runner/TestRunner.java
+++ src/test/java/br/com/rsinet/bdd/mobile/appium/runner/TestRunner.java
@@ -15,24 +15,18 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(features = "src/test/resources/Feature", glue = { "br.com.rsinet.bdd.mobile.appium.steps" },
 //tags = {"@telainicial1,@telainicial1 "},
 tags = {"@tag2"},
-plugin = {"pretty" , "json:target/cucumber-reports/json-report.json", "com.cucumber.listener.ExtentCucumberFormatter:relatorio/report.html"}, 
+plugin = {"pretty","com.cucumber.listener.ExtentCucumberFormatter:C:\\Users\\joice.abreu\\Desktop\\RSI_HUB_MOBILE_APPIUM_BDD\\target\\cucumber-reports\\report.html"}, 
 snippets = SnippetType.CAMELCASE, 
-dryRun = false, 
-strict = false,
 monochrome = true)
 
 public class TestRunner {
 	
 	@AfterClass
-	public static void ExtentReport() {
+	public static  void initReport() {
 	
 		Reporter.loadXMLConfig(new File("src/test/resources/config/extension-config.xml"));;
 		Reporter.setSystemInfo("User Name", System.getProperty("Joice.Abreu"));
 		Reporter.setSystemInfo("Time zone", System.getProperty("user.timezone"));
-		Reporter.setSystemInfo("Machine", "Windows 10" + "64 Bit");
-		Reporter.setSystemInfo("cucumber-java", "1.2.5");
-		Reporter.setSystemInfo("cucumber-junit", "1.2.5");
-		Reporter.setSystemInfo("Maven", "4.0.0");
 
 	}
 }
